317128a5a82677da8e43d731642203f55353b242
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-01-21 00:03:57 +0700

    add ExcelConstant and MessageConstant classes

---
+++ src/main/java/com/hatester/constants/ExcelConstant.java
@@ -0,0 +1,12 @@
+package com.hatester.constants;
+
+public class ExcelConstant {
+    //file name
+    public static final String EXCEL_FILE_NAME = "dataCRM.xlsx";
+
+    //sheet name
+    public static final String EXCEL_SHEET_LOGIN = "Login";
+    public static final String EXCEL_SHEET_CUSTOMERS = "Customers";
+    public static final String EXCEL_SHEET_PROJECTS = "Projects";
+    public static final String EXCEL_SHEET_TASKS = "Tasks";
+}
---
+++ src/main/java/com/hatester/constants/MessageConstant.java
@@ -0,0 +1,6 @@
+package com.hatester.constants;
+
+public class MessageConstant {
+    //login
+    public static final String INVALID_EMAIL_OR_PASSWORD = "Invalid email or password";
+}
