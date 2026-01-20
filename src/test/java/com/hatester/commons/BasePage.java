6ecbba2785e840a954d06731341f24fb0d201df5
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-01-21 00:08:40 +0700

    add locator for Dashboard menu in BasePage

---
+++ src/test/java/com/hatester/commons/BasePage.java
@@ -0,0 +1,7 @@
+package com.hatester.commons;
+
+import org.openqa.selenium.By;
+
+public class BasePage {
+    public By menuDashboard = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Dashboard']");
+}
