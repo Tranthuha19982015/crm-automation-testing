431f31e33077a8096c1bc3f0943179e71d4e31ff
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-01-22 10:40:17 +0700

    add menu locators and navigation methods in BasePage

--- src/test/java/com/hatester/commons/BasePage.java
+++ src/test/java/com/hatester/commons/BasePage.java
@@ -1,7 +1,19 @@
 package com.hatester.commons;
 
+import com.hatester.crm.pages.CustomerPage;
+import com.hatester.keywords.WebUI;
 import org.openqa.selenium.By;
 
 public class BasePage {
-    public By menuDashboard = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Dashboard']");
+    protected By menuDashboard = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Dashboard']");
+    protected By menuCustomers = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Customers']");
+
+    protected void clickMenuItem(By element) {
+        WebUI.clickElement(element);
+    }
+
+    public CustomerPage goToCustomersFromMenu() {
+        clickMenuItem(menuCustomers);
+        return new CustomerPage();
+    }
 }
