7f110e203e885263445a30361106308b3199bf46
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-01-24 23:55:12 +0700

    add Project menu locators and navigation methods in BasePage

--- src/test/java/com/hatester/commons/BasePage.java
+++ src/test/java/com/hatester/commons/BasePage.java
@@ -1,12 +1,14 @@
 package com.hatester.commons;
 
 import com.hatester.crm.pages.CustomerPage;
+import com.hatester.crm.pages.ProjectPage;
 import com.hatester.keywords.WebUI;
 import org.openqa.selenium.By;
 
 public class BasePage {
     protected By menuDashboard = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Dashboard']");
     protected By menuCustomers = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Customers']");
+    protected By menuProjects = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Projects']");
 
     protected void clickMenuItem(By element) {
         WebUI.clickElement(element);
@@ -15,5 +17,10 @@ public class BasePage {
     public CustomerPage goToCustomersFromMenu() {
         clickMenuItem(menuCustomers);
         return new CustomerPage();
+    }
+
+    public ProjectPage goToProjectsFromMenu() {
+        clickMenuItem(menuProjects);
+        return new ProjectPage();
     }
 }
