c94176f0eda1bf5e2d972fb83ade1bd0577a91b0
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-02-04 10:09:37 +0700

    add Tasks menu locator and navigation method

--- src/test/java/com/hatester/commons/BasePage.java
+++ src/test/java/com/hatester/commons/BasePage.java
@@ -2,6 +2,7 @@ package com.hatester.commons;
 
 import com.hatester.crm.pages.CustomerPage;
 import com.hatester.crm.pages.ProjectPage;
+import com.hatester.crm.pages.TaskPage;
 import com.hatester.keywords.WebUI;
 import org.openqa.selenium.By;
 
@@ -9,6 +10,7 @@ public class BasePage {
     protected By menuDashboard = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Dashboard']");
     protected By menuCustomers = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Customers']");
     protected By menuProjects = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Projects']");
+    protected By menuTasks = By.xpath("//ul[@id='side-menu']/descendant::a[normalize-space()='Tasks']");
 
     protected void clickMenuItem(By element) {
         WebUI.clickElement(element);
@@ -22,5 +24,10 @@ public class BasePage {
     public ProjectPage goToProjectsFromMenu() {
         clickMenuItem(menuProjects);
         return new ProjectPage();
+    }
+
+    public TaskPage goToTasksFromMenu() {
+        clickMenuItem(menuTasks);
+        return new TaskPage();
     }
 }
