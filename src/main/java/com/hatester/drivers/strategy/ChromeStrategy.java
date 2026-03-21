0d7d154bd9d01f6358a710d34bf58e633718a7f4
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-03-21 20:56:11 +0700

    improve ChromeOptions by using prefs to disable password manager

--- src/main/java/com/hatester/drivers/strategy/ChromeStrategy.java
+++ src/main/java/com/hatester/drivers/strategy/ChromeStrategy.java
@@ -7,16 +7,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChromeStrategy extends AbstractBrowserStrategy {
     @Override
     public WebDriver createDriver() {
-        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
 
-        // Disable Chrome popup
-        options.addArguments("--disable-save-password-bubble");
-        options.addArguments("--disable-features=PasswordLeakDetection");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-infobars");
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
 
         if (isHeadless()) {
             options.addArguments("--headless=new");
